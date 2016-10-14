257095cced22041531f8a2ab7e74bba4d5a20c84
prasanth0023 <prasanthpayne@gmail.com>
2016-10-14 22:37:53 +0530

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-
-gulp.task('default', () =>
-   gulp.src('views/images/*')
-       .pipe(imagemin())
-       .pipe(gulp.dest('dist/images'))
-);
